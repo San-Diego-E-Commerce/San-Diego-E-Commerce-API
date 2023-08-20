989dde57f20b484b60e42303424765b970f284ba
lfeitosaf <lucas-jpl@hotmail.com>
2023-08-20 20:32:09 -0300

    add entity/model user

---
+++ src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,29 @@
+import { hashSync } from 'bcryptjs';
+import { Transform } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  first_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  last_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @Transform(({ value }: { value: string }) => hashSync(value, 10), {
+    groups: ['transform'],
+  })
+  password: string;
+}
